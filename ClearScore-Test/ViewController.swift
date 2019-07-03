49d9611ab7f7a423f277851ce4ac7968cc2b8e1c
Jacob Whitehead <jacobawhitehead@gmail.com>
2019-07-03 12:21:32 +0100

    Initial Commit

---
+++ ClearScore-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearScore-Test
+//
+//  Created by Jacob Whitehead on 03/07/2019.
+//  Copyright © 2019 Jacob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
